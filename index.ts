42980cea63a3a42d3b66d15ca31b67fe98565a7e
Ronak Ruparel <ronakruparel@ALIPL5283s-MacBook-Pro.local>
2020-05-12 13:27:18 +0530

    removed logs

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
 const {execute} = require("./gql.ts");
 
 module.exports.gql = async (input) => {
-  console.log(input);
   const data = await execute(input);
-  console.log(data);
   return data;
 };
---
+++ test.ts
@@ -0,0 +1,3 @@
+const {gql} = require("./index.ts");
+
+gql();
