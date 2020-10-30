ed60f05612a854e6958f66de0b82330ab22ca71f
Ronak Ruparel <ronakruparel@ALIPL5283s-MacBook-Pro.local>
2020-10-30 15:47:45 +0530

    feat(mapper): added lodash for null values

--- mapper/mapper.ts
+++ mapper/mapper.ts
@@ -1,15 +1,16 @@
 const gcalConfig = require("../config/gCalConfig.json");
 const calendarEvent = require("../schema/celandarSchema.json");
+const _ = require("lodash")
 module.exports.mapper = (payload) => {
   let finalResp = [];
-
   let config = gcalConfig;
+  let newResp 
   for (let i = 0; i < payload.length; i++) {
+    newResp = _.cloneDeep(calendarEvent)
     finalResp.push(iterate(payload[i], ""));
   }
 
   function iterate(obj, stack) {
-    let newResp = {};
     for (let property in obj) {
       if (typeof obj[property] == "object") {
         iterate(obj[property], stack + "." + property);
