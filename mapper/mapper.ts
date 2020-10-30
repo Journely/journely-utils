886243f3885722760f4765b53f9b21d24fbebe1c
Ronak Ruparel <ronakruparel@ALIPL5283s-MacBook-Pro.local>
2020-10-30 17:05:52 +0530

    fix(mapper): fixed array type issue

--- mapper/mapper.ts
+++ mapper/mapper.ts
@@ -5,26 +5,34 @@ const calendarEvent = require("../schema/celandarSchema.json");
 const configMapper ={
   calendar:gcalConfig,
 }
+
 //mappper for all schemas
 const schemaMapper ={
   calendar:calendarEvent
 }
+
 const _ = require("lodash")
 module.exports.mapper = (payload,schemaName) => {
-  console.log(schemaName)
+
   let newResp 
   let finalResp = [];
   //map appropriate config based on name
   let config = configMapper[schemaName];
   for (let i = 0; i < payload.length; i++) {
     // map appropriate schema based on name
-    newResp = _.cloneDeep(schemaMapper[schemaName])
-    finalResp.push(iterate(payload[i], ""));
+    newResp = _.cloneDeep(calendarEvent);
+    let result = iterate(payload[i], "");
+    for (let [key, value] of Object.entries(result)) {
+      if (value !== undefined && (value === "string" || value === "number" || value === "datetime" || value === "boolean") || value === "array" || value === "object") {
+        result[key] = null;
+      }
+    }
+    finalResp.push(result);
   }
 
   function iterate(obj, stack) {
     for (let property in obj) {
-      if (typeof obj[property] == "object") {
+      if (!Array.isArray(obj[property]) && typeof obj[property] == "object") {
         iterate(obj[property], stack + "." + property);
       } else {
         let key = stack + "." + property;
